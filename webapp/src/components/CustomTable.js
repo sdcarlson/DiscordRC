9f97c0084e9a48f5dd64685d9f8b97536d664719
g2su <g2su@ucsd.edu>
2024-02-16 12:19:03 -0800

    pages + context

--- webapp/src/components/CustomTable.js
+++ webapp/src/components/CustomTable.js
@@ -1,38 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Container, Stack, Button, Checkbox } from '@mui/material'
+import { useFormContext } from '../context/FormContext'
 import CustomTableRowDialog from './CustomTableRowDialog'
 
 const CustomTable = (props) => {
 
-  // ['name', 'display_separately', 'allow_mention', 'roleperm1', 'roleperm2']
-  const [rows, setRows] = useState([{
-    name: 'temp',
-    display_separately: 'False',
-    allow_mention: 'False',
-    roleperm1: 'True',
-    roleperm2: 'True'
-  }, {
-    name: 'temp2',
-    display_separately: 'True',
-    allow_mention: 'True',
-    roleperm1: 'True',
-    roleperm2: 'True'
-  }, {
-    name: 'temp3',
-    display_separately: 'True',
-    allow_mention: 'False',
-    roleperm1: 'False',
-    roleperm2: 'True'
-  }, {
-    name: 'temp4',
-    display_separately: 'True',
-    allow_mention: 'True',
-    roleperm1: 'True',
-    roleperm2: 'False'
-  }]) // FIXME: replace with []
+  const {
+    roleData,
+    setRoleData
+  } = useFormContext()
+
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    if (props.context === 'role')
+      setRows(roleData)
+  }, [])
+
+  useEffect(() => {
+    if (props.context === 'role')
+      setRoleData(rows);
+  }, [rows])
     
   const [checkbox, setCheckbox] = useState([])
   const [openAddDialog, setOpenAddDialog] = useState(false);
+  const [openEditDialog, setOpenEditDialog] = useState(false);
 
   const handleParentCheckbox = (event) => {
     if (event.target.checked) 
@@ -58,10 +50,13 @@ const CustomTable = (props) => {
     <Container sx={{ padding: 5 }}>
       <CustomTableRowDialog rows={rows} setRows={setRows} open={openAddDialog} setOpen={setOpenAddDialog} 
         headings={props.headings} rownames={props.rownames} rowtypes={props.rowtypes} title='Add Role'/>
+      <CustomTableRowDialog rows={rows} setRows={setRows} open={openAddDialog} setOpen={setOpenAddDialog} 
+        headings={props.headings} rownames={props.rownames} rowtypes={props.rowtypes} title='Edit Role'/>
       <Stack spacing={5} direction="row" justifyContent="center">
         <Button variant="contained" style={{maxWidth: '175px', maxHeight: '40px', minWidth: '175px', minHeight: '40px'}} 
         onClick={()=>{setOpenAddDialog(true);}}>Add Row</Button>
-        <Button variant="contained" style={{maxWidth: '175px', maxHeight: '40px', minWidth: '175px', minHeight: '40px'}}>Edit Row</Button>
+        <Button variant="contained" style={{maxWidth: '175px', maxHeight: '40px', minWidth: '175px', minHeight: '40px'}} 
+        onClick={()=>{setOpenEditDialog(true);}}>Edit Row</Button>
         <Button variant="contained" style={{maxWidth: '175px', maxHeight: '40px', minWidth: '175px', minHeight: '40px'}} 
           onClick={deleteSelectedRow}>Delete Row(s)</Button>
       </Stack>
--- webapp/src/context/FormContext.js
+++ webapp/src/context/FormContext.js
@@ -4,111 +4,34 @@ const FormContext = createContext({})
 
 export const FormProvider = ({ children }) => {
 
-    const title = {
-        // 0: 'Welcome',
-        // 1: 'Shipping Info',
-        // 2: 'Opt-In'
-    }
-
-    const [page, setPage] = useState(0)
-
-    const [formData, setFormData] = useState({
-        temp: "temp",
-        // billFirstName: "",
-        // billLastName: "",
-        // billAddress1: "",
-        // billAddress2: "",
-        // billCity: "",
-        // billState: "",
-        // billZipCode: "",
-        // sameAsBilling: false,
-        // shipFirstName: "",
-        // shipLastName: "",
-        // shipAddress1: "",
-        // shipAddress2: "",
-        // shipCity: "",
-        // shipState: "",
-        // shipZipCode: "",
-        // optInNews: false
-    })
-
-
-    // useEffect(() => {
-    //     if (data.sameAsBilling) {
-    //         setData(prevData => ({
-    //             ...prevData,
-    //             shipFirstName: prevData.billFirstName,
-    //             shipLastName: prevData.billLastName,
-    //             shipAddress1: prevData.billAddress1,
-    //             shipAddress2: prevData.billAddress2,
-    //             shipCity: prevData.billCity,
-    //             shipState: prevData.billState,
-    //             shipZipCode: prevData.billZipCode
-    //         }))
-    //     } else {
-    //         setData(prevData => ({
-    //             ...prevData,
-    //             shipFirstName: "",
-    //             shipLastName: "",
-    //             shipAddress1: "",
-    //             shipAddress2: "",
-    //             shipCity: "",
-    //             shipState: "",
-    //             shipZipCode: ""
-    //         }))
-    //     }
-    // }, [data.sameAsBilling])
-
-    // const handleFormChange = e => {
-    //     const type = e.target.type
-
-    //     const name = e.target.name
-
-    //     const value = type === "checkbox"
-    //         ? e.target.checked
-    //         : e.target.value
-
-    //     setData(prevData => ({
-    //         ...prevData,
-    //         [name]: value
-    //     }))
-    // }
-
-    // const {
-    //     billAddress2,
-    //     sameAsBilling,
-    //     shipAddress2,
-    //     optInNews,
-    //     ...requiredInputs } = data
-
-    // const canSubmit = [...Object.values(requiredInputs)].every(Boolean) && page === Object.keys(title).length - 1
-
-    // const canNextPage1 = Object.keys(data)
-    //     .filter(key => key.startsWith('bill') && key !== 'billAddress2')
-    //     .map(key => data[key])
-    //     .every(Boolean)
-
-    // const canNextPage2 = Object.keys(data)
-    //     .filter(key => key.startsWith('ship') && key !== 'shipAddress2')
-    //     .map(key => data[key])
-    //     .every(Boolean)
-
-    // const disablePrev = page === 0
-
-    // const disableNext =
-    //     (page === Object.keys(title).length - 1)
-    //     || (page === 0 && !canNextPage1)
-    //     || (page === 1 && !canNextPage2)
-
-    // const prevHide = page === 0 && "remove-button"
-
-    // const nextHide = page === Object.keys(title).length - 1 && "remove-button"
-
-    // const submitHide = page !== Object.keys(title).length - 1 && "remove-button"
+    const [roleData, setRoleData] = useState([{
+        name: 'temp',
+        display_separately: 'False',
+        allow_mention: 'False',
+        roleperm1: 'True',
+        roleperm2: 'True'
+      }, {
+        name: 'temp2',
+        display_separately: 'True',
+        allow_mention: 'True',
+        roleperm1: 'True',
+        roleperm2: 'True'
+      }, {
+        name: 'temp3',
+        display_separately: 'True',
+        allow_mention: 'False',
+        roleperm1: 'False',
+        roleperm2: 'True'
+      }, {
+        name: 'temp4',
+        display_separately: 'True',
+        allow_mention: 'True',
+        roleperm1: 'True',
+        roleperm2: 'False'
+    }]) // FIXME: temp replace with []
 
     return (
-        //canSubmit, handleFormChange, disablePrev, disableNext, prevHide, nextHide, submitHide }}>
-        <FormContext.Provider value={{ title, page, setPage, formData, setFormData }}> 
+        <FormContext.Provider value={{ roleData, setRoleData }}> 
             {children}
         </FormContext.Provider>
     )
