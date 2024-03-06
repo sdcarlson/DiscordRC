d577b5048e91c43feaf5ffaf9d38644ae924f593
Quan Do <biquando@gmail.com>
2024-03-06 14:40:40 -0800

    Clean up API generated documentation

--- api/main.py
+++ api/main.py
@@ -6,11 +6,14 @@ from fastapi import FastAPI
 import api.documentation
 from api.routes import auth, users, config, bot
 
-app = FastAPI()
+app = FastAPI(
+    title='DiscordRC',
+    description='This is the documentation for the backend API of DiscordRC.'
+)
 
 app.include_router(auth.router, prefix='/auth')
 app.include_router(users.router, prefix='/users')
 app.include_router(config.router, prefix='/config')
 app.include_router(bot.router, prefix='/bot')
 
-api.documentation.generate(app, 'documentation/documentation.html')
+api.documentation.generate(app, 'documentation/api.html')
