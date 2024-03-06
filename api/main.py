ba236f2baa50d9ea163596d7e7f5f185032a3c04
Quan Do <biquando@gmail.com>
2024-03-06 13:14:15 -0800

    Generate static API documentation

--- api/main.py
+++ api/main.py
@@ -3,6 +3,7 @@ Main file for the FastAPI backend.
 '''
 
 from fastapi import FastAPI
+import api.documentation
 from api.routes import auth, users, config, bot
 
 app = FastAPI()
@@ -11,3 +12,5 @@ app.include_router(auth.router, prefix='/auth')
 app.include_router(users.router, prefix='/users')
 app.include_router(config.router, prefix='/config')
 app.include_router(bot.router, prefix='/bot')
+
+api.documentation.generate(app, 'documentation/documentation.html')
