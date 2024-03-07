7b9befcc69e858f2f4a5217d13dfb3d73e59bd2d
Quan Do <biquando@gmail.com>
2024-03-06 18:12:25 -0800

    Add error msg if trying to run API the old way

--- api/main.py
+++ api/main.py
@@ -2,8 +2,22 @@
 Main file for the FastAPI backend.
 '''
 
+try:
+    import api.documentation
+except ModuleNotFoundError:
+    ERROR_MSG = \
+'''\033[91mERROR\033[0m: You didn't run this correctly. While you're inside of the `api/`
+directory, you should run:
+
+sh -c 'cd .. && uvicorn api.main:app --reload'
+
+In PR #33, I changed all the import paths so that `DiscordRC/` is the root
+directory. This lets us use the code in `DiscordBot/`. For more information,
+see `api/README.md` or message me (biquando) on Discord.'''
+    print(ERROR_MSG)
+    exit(1)
+
 from fastapi import FastAPI
-import api.documentation
 from api.routes import auth, users, config, bot
 
 app = FastAPI(
